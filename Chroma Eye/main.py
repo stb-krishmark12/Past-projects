cb4bdd41305cb4690392abe1cf57d8e98b81219b
Krishna kumar <skk12112003@gmail.com>
2022-03-25 21:46:40 +0530

    Add files via upload

---
+++ Chroma Eye/main.py
@@ -0,0 +1,41 @@
+# Python program for Detection of a
+# specific color(red here) using OpenCV with Python
+import cv2
+import numpy as np
+
+# Webcamera no 0 is used to capture the frames
+cap = cv2.VideoCapture(0)
+
+# This drives the program into an infinite loop.
+while (1):
+    # Captures the live stream frame-by-frame
+    _, frame = cap.read()
+    # Converts images from BGR to HSV66
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([161, 155, 84])
+    upper_red = np.array([179,255,255])
+
+    # Here we are defining range of redcolor in HSV
+    # This creates a mask of red coloured
+    # objects found in the frame.
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # The bitwise and of the frame and mask is done so
+    # that only the red coloured objects are highlighted
+    # and stored in res
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+
+    # This displays the frame, mask
+    # and res which we created in 3 separate windows.
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+# Destroys all of the HighGUI windows.
+cv2.destroyAllWindows()
+
+# release the captured frame
+cap.release()
